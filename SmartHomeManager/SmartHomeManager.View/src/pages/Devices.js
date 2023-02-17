7ff04c77fcf03599d53f46114ecd24e75cfa70e1
ChenYJ-SIT <2102058@sit.singaporetech.edu.sg>
2023-02-17 16:50:08 +0800

    front end fix

--- SmartHomeManager/SmartHomeManager.View/src/pages/Devices.js
+++ SmartHomeManager/SmartHomeManager.View/src/pages/Devices.js
@@ -1,5 +1,5 @@
 import React from "react";
-import DeviceLogging from "components/DeviceLogging";
+import DeviceLogging from "components/DeviceLogging/DeviceLogging";
 function Devices() {
     return (
         <div className="container mt-3">
