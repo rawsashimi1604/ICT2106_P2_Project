e3a552a15879b713b5012cf4f83d3baf1f7d665e
junweilam <91267859+junweilam@users.noreply.github.com>
2023-03-03 18:13:01 +0800

    download button pdf working

--- SmartHomeManager/SmartHomeManager.View/src/components/Analytics/Report.js
+++ SmartHomeManager/SmartHomeManager.View/src/components/Analytics/Report.js
@@ -14,6 +14,14 @@ import ReportService from 'requests/services/ReportService'
 const handleClick = () => {
     // handling the post request...
     ReportService.getDeviceReport()
+        .then((response) =>{
+            const url = window.URL.createObjectURL(new Blob([response.data]));
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', 'MyPdfReport.pdf');
+            document.body.appendChild(link);
+            link.click();
+    });
 }
 
 function EfficiencyTable() {
--- SmartHomeManager/SmartHomeManager.View/src/requests/services/ReportService.js
+++ SmartHomeManager/SmartHomeManager.View/src/requests/services/ReportService.js
@@ -1,9 +1,9 @@
-import HttpService from "requests/HttpService"
+import PdfDownloadService from "requests/PdfDownloadService"
 
 const GET_DEVICE_REPORT_API_ROUTE = `https://localhost:7140/api/analysis/device/download`
 
 function getDeviceReport(){
-    return HttpService.get(GET_DEVICE_REPORT_API_ROUTE);
+    return PdfDownloadService.get(GET_DEVICE_REPORT_API_ROUTE);
 }
 
 export default{
