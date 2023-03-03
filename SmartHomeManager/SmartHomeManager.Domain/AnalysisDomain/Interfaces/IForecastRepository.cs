ea077496f82cad517c57de328e315a106f1a7642
nicnathaniel <nicholasquek98@gmail.com>
2023-03-03 17:01:51 +0800

    added Iforecast repository

---
+++ SmartHomeManager/SmartHomeManager.Domain/AnalysisDomain/Interfaces/IForecastRepository.cs
@@ -0,0 +1,16 @@
+﻿using SmartHomeManager.Domain.AnalysisDomain.Entities;
+using SmartHomeManager.Domain.Common;
+using SmartHomeManager.Domain.NotificationDomain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeManager.Domain.AnalysisDomain.Interfaces
+{
+    public interface IForecastRepository : IGenericRepository<ForecastChart>
+    {
+        public Task<IEnumerable<Notification>> GetAllByIdAsync(Guid id);
+    }
+}
