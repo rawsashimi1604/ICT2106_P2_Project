6ccf025a1bfcf8f1735da60dfb82a7eb1e9ab965
Gavin Loo <looweiren@gmail.com>
2023-03-25 21:06:40 +0800

    fixed carbon footprint frontend data loading errors

--- SmartHomeManager/SmartHomeManager.View/src/requests/services/CarbonFootprintService.js
+++ SmartHomeManager/SmartHomeManager.View/src/requests/services/CarbonFootprintService.js
@@ -5,6 +5,7 @@ const GET_CARBON_FOOTPRINT_DATA_ROUTE = `https://localhost:7140/api/analysis/hou
 
 function getCarbonFootprintData(accountId, year, month){
 
+    console.log(`${GET_CARBON_FOOTPRINT_DATA_ROUTE}${accountId}/${year}-${month}`)
     return HttpService.get(`${GET_CARBON_FOOTPRINT_DATA_ROUTE}${accountId}/${year}-${month}`)
 }
 
