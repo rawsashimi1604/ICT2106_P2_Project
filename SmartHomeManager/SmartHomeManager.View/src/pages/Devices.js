1947b41d8a3bb6f0b49199c6677f8fff99d6ed2c
Geethika <71939490+geethika-m@users.noreply.github.com>
2023-03-27 15:48:59 +0800

    moved set limit to devices

--- SmartHomeManager/SmartHomeManager.View/src/pages/Devices.js
+++ SmartHomeManager/SmartHomeManager.View/src/pages/Devices.js
@@ -1,9 +1,12 @@
 import React from "react";
 import DeviceLogging from "components/DeviceLogging/DeviceLogging";
+import DeviceEnergyLimitTable from "pages/DeviceEnergyLimit/EnergyLimitTable";
+import EnergyLimitTable from "pages/DeviceEnergyLimit/EnergyLimitTable";
 function Devices() {
     return (
         <div className="container mt-3">
             <DeviceLogging />
+            <EnergyLimitTable/>
         </div>
     );
 }
